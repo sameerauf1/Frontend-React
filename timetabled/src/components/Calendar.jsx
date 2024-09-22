98b81036c898d674c432ed86e1edc90e5d921329
Samee Rauf <sameerauf1@gmail.com>
2024-09-22 10:39:28 -0500

    created calendar template

---
+++ timetabled/src/components/Calendar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Calendar = () => {
+    return (
+        <div className="Calendar" > 
+        <text>Testing the Calendar Component</text> 
+        </div>
+    )
+}
+export default Calendar
